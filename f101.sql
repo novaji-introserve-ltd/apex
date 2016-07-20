3d3a1665c9e51f4d80eee59ccd348b68ea595e53
Novaji Support <support@novajii.com>
2016-07-21 00:00:25 +0100

    APEX Application Files: Thursday 29 2016 12:00:01 AM

--- f101.sql
+++ f101.sql
@@ -27,7 +27,7 @@ prompt APPLICATION 101 - easytaxpayer.com
 -- Application Export:
 --   Application:     101
 --   Name:            easytaxpayer.com
---   Date and Time:   00:00 Wednesday July 20, 2016
+--   Date and Time:   00:00 Thursday July 21, 2016
 --   Exported By:     ORE
 --   Flashback:       0
 --   Export Type:     Application Export
--- f111.sql
+++ f111.sql
@@ -27,7 +27,7 @@ prompt APPLICATION 111 - ore.ng
 -- Application Export:
 --   Application:     111
 --   Name:            ore.ng
---   Date and Time:   00:00 Wednesday July 20, 2016
+--   Date and Time:   00:00 Thursday July 21, 2016
 --   Exported By:     ORE
 --   Flashback:       0
 --   Export Type:     Application Export
